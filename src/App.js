98a0939e362694a53198b275903db34b796fd578
jjs951213 <jjs951213@naver.com>
2020-09-12 04:57:40 +0900

    12.1.5 useState의 함수형 업데이트와 immer함께쓰기

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     e => {
       const { name, value } = e.target;
       setForm({
-        produce(form, draft => {
+        produce(draft => {
           draft[name] = value;
       })
     },
@@ -37,7 +37,7 @@ const App = () => {
       };
 
   setData({
-    produce(data, draft => {
+    produce(draft => {
       draft.array.push(info);
     })
 
@@ -63,7 +63,7 @@ const App = () => {
   const onRemove = useCallback(
     id => {
       setData({
-        produce(data, draft => {
+        produce(draft => {
           draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
         })
     },
